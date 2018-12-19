253a3e70fce64bd1693dc57d33a8e52a2604ee1e
kzkick2nd <aruga.kazuki@gmail.com>
2018-12-20 02:44:35 +0900

    Add auth code

--- vue-auth/src/components/Hello.vue
+++ vue-auth/src/components/Hello.vue
@@ -1,52 +1,42 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="login">login</button>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
+
+// コンソールから取得したコンフィグをペースト
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyC_yoWZfT-XHZda_PAuyIA-witVkbxjihU',
+  authDomain: 'auth-vue-sample.firebaseapp.com',
+  databaseURL: 'https://auth-vue-sample.firebaseio.com',
+  projectId: 'auth-vue-sample',
+  storageBucket: 'auth-vue-sample.appspot.com',
+  messagingSenderId: '1011538488281'
+}
+firebase.initializeApp(config)
+
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js PWA'
     }
+  },
+  methods: {
+    login () {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase.auth().signInWithPopup(provider)
+        .catch(error => alert(error.message))
+        .then(data => {
+          // ここにログインできたときの処理を書く
+          alert('login success')
+          console.log(data)
+        })
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495E;
-}
-</style>
